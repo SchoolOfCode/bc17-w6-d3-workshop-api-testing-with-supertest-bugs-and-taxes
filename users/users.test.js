2d1dee1e780496e649bfa8657b44ff241c6d3815
irena binnebesel <irenabinnebesel1@gmail.com>
2024-08-07 16:51:50 +0100

    running tests

--- users/users.test.js
+++ users/users.test.js
@@ -1,6 +1,9 @@
-import {expect, test} from "vitest";
+import {expect, test, expectTypeOf} from "vitest";
 import request from "supertest";
-import app from "../app.js"
+import app from "../app.js";
+import {seedData} from "../db/seed-data.js";
+import{resetUsersTable} from "../db/helpers.js";
+
 
 // Goal: make a GET request with Supertest to the /api/health
 //
@@ -42,3 +45,38 @@ test("GET /api/health works", () => {
       });
   });
 
+  // write a test skeleton with a descriptive test name ("GET /api/users" could be a starting point) done
+
+   test("GET/api/users", async  function () {
+   const dbReset = await resetUsersTable(seedData);
+   const response = await request(app).get("/api/users");
+   expect(response.headers["content-type"]).toMatch(/json/);
+   expect(response.status).toEqual(200);
+   expect(response.body).toBeTypeOf("object");
+   expect(response.body.success).toBe(true);
+   expectTypeOf(response.body.payload).toBeArray(); 
+
+
+   })
+    
+// run tests to make sure the skeleton passes on its own done
+
+// then within the test:
+//    ARRANGE:
+//      use the `resetUsersTable` function to reset the database table to a known state done
+
+//    ACT:
+//      use Supertest to send a GET request to the `/api/users` endpoint
+//      wait for the response done
+//    ASSERT:
+//      assert that the response body is an object done
+//      assert that response body.success is true done
+//      assert that response body.payload is an array done
+//      loop over the payload array. for each user object in the payload array:
+//          assert that user object.id is a number
+//          assert that user object.username is a string
+//      assert that the response status code is 200
+//      assert that there's a Content-Type response header which contains `application/json`
+//      any other assertions that you think would be useful
+// run tests to ensure they passes
+// temporarily break the implementation in `users/users.controller.js` to ensure test fails and then change back so that tests pass
